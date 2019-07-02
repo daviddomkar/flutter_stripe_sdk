120bb4c410c64155abd574047ce31060ee9c99fd
David Domkář <dave.domkar@gmail.com>
2019-07-03 00:16:55 +0200

    Init method

--- android/src/main/kotlin/io/dexdev/flutter_stripe/FlutterStripePlugin.kt
+++ android/src/main/kotlin/io/dexdev/flutter_stripe/FlutterStripePlugin.kt
@@ -1,25 +1,39 @@
 package io.dexdev.flutter_stripe
 
+import android.app.Activity
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.Stripe
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterStripePlugin: MethodCallHandler {
+class FlutterStripePlugin(private val activity: Activity): MethodCallHandler {
+
+  private lateinit var stripe: Stripe
+
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
       val channel = MethodChannel(registrar.messenger(), "flutter_stripe")
-      channel.setMethodCallHandler(FlutterStripePlugin())
+      channel.setMethodCallHandler(FlutterStripePlugin(registrar.activity()))
     }
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "init") {
+      init(call.argument<String>("publishableKey")!!)
+      result.success(null)
     } else {
       result.notImplemented()
     }
   }
+
+  private fun init(publishableKey: String) {
+    PaymentConfiguration.init(publishableKey)
+    stripe = Stripe(activity, PaymentConfiguration.getInstance().publishableKey)
+  }
+
+
 }
--- lib/flutter_stripe.dart
+++ lib/flutter_stripe.dart
@@ -3,8 +3,11 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterStripe {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_stripe');
+  static const MethodChannel _channel = const MethodChannel('flutter_stripe');
+
+  static Future<void> init(String publishableKey) async {
+    await _channel.invokeListMethod('init', publishableKey);
+  }
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
