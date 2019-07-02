9d236a858dfbea135da17c7999ae704f15282a08
David Domkář <dave.domkar@gmail.com>
2019-07-03 01:02:27 +0200

    iOS init method

--- ios/Classes/SwiftFlutterStripePlugin.swift
+++ ios/Classes/SwiftFlutterStripePlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Stripe
 
 public class SwiftFlutterStripePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,6 +10,20 @@ public class SwiftFlutterStripePlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+      case "init":
+        _init(publishableKey: (call.arguments as! Dictionary<String, AnyObject>)["publishableKey"] as! String)
+        result(nil)
+        break
+      default:
+        result(FlutterMethodNotImplemented);
+        break
+    }
+    
     result("iOS " + UIDevice.current.systemVersion)
+  }
+    
+  private func _init(publishableKey: String) {
+    STPPaymentConfiguration.shared().publishableKey = publishableKey
   }
 }
