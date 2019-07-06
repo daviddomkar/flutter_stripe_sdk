a17aa5a951ed7aec80d48d6bf7efc6615c80666e
David Domkář <dave.domkar@gmail.com>
2019-07-06 02:36:13 +0200

    iOS implementation

--- ios/Classes/SwiftFlutterStripePlugin.swift
+++ ios/Classes/SwiftFlutterStripePlugin.swift
@@ -3,9 +3,19 @@ import UIKit
 import Stripe
 
 public class SwiftFlutterStripePlugin: NSObject, FlutterPlugin {
+  private let ephemeralKeyProvider: EphemeralKeyProvider;
+  private let methodChannel: FlutterMethodChannel;
+  
+  private var customerSession: STPCustomerContext?;
+  
+  init(methodChannel: FlutterMethodChannel) {
+    self.methodChannel = methodChannel
+    self.ephemeralKeyProvider = EphemeralKeyProvider(methodChannel: methodChannel)
+  }
+  
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_stripe", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterStripePlugin()
+    let instance = SwiftFlutterStripePlugin(methodChannel: channel)
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
@@ -15,15 +25,55 @@ public class SwiftFlutterStripePlugin: NSObject, FlutterPlugin {
         _init(publishableKey: (call.arguments as! Dictionary<String, AnyObject>)["publishableKey"] as! String)
         result(nil)
         break
-      default:
-        result(FlutterMethodNotImplemented)
-        break
+    case "initCustomerSession":
+      _initCustomerSession();
+      result(nil)
+      break
+    case "onKeyUpdate":
+      ephemeralKeyProvider.keyUpdateListener?((call.arguments as! Dictionary<String, AnyObject>)["stripeResponseJson"] as? [String: AnyObject], nil)
+      
+      result(nil)
+      break
+    case "onKeyUpdateFailure":
+      ephemeralKeyProvider.keyUpdateListener?(nil, NSError(domain: "flutter_stripe", code: (call.arguments as! Dictionary<String, AnyObject>)["responseCode"] as! Int, userInfo: ["message": (call.arguments as! Dictionary<String, AnyObject>)["message"] as! String]))
+
+      result(nil)
+      break
+    case "endCustomerSession":
+      _endCustomerSession()
+      result(nil)
+      break
+    default:
+      result(FlutterMethodNotImplemented)
+      break
     }
-    
-    result("iOS " + UIDevice.current.systemVersion)
   }
     
   private func _init(publishableKey: String) {
     STPPaymentConfiguration.shared().publishableKey = publishableKey
+  }
+  
+  private func _initCustomerSession() {
+    customerSession = STPCustomerContext(keyProvider: ephemeralKeyProvider)
+  }
+  
+  private func _endCustomerSession() {
+    customerSession?.clearCachedCustomer()
+    customerSession = nil;
+  }
+}
+
+private class EphemeralKeyProvider: NSObject, STPEphemeralKeyProvider {
+  private let methodChannel: FlutterMethodChannel;
+
+  public var keyUpdateListener: STPJSONResponseCompletionBlock?;
+  
+  init (methodChannel: FlutterMethodChannel) {
+    self.methodChannel = methodChannel;
+  }
+  
+  func createCustomerKey(withAPIVersion apiVersion: String, completion: @escaping STPJSONResponseCompletionBlock) {
+    keyUpdateListener = completion;
+    methodChannel.invokeMethod("createEphemeralKey", arguments: ["apiVersion": apiVersion])
   }
 }
